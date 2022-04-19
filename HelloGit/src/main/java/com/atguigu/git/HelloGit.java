25506db381146cb833b5c432bf69c248fc4f76a3
tom_glb <you@example.com>
2022-04-19 23:53:54 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -7,6 +7,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit!");
+        System.out.println("更新1");
     }
-
 }
