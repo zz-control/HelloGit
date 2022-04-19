5c342d98066cd6bbfdcf7e10eb65fb919e34a32b
tom_glb <you@example.com>
2022-04-19 23:52:47 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author fight
+ * @create 2022-04-19 23:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+    }
+
+}
